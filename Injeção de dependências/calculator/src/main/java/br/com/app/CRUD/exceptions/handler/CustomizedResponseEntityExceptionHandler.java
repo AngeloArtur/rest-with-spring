8c9d7fecfacf04d82766e9d13821f0e94567418c
Ângelo Artur <ang.vianna@gmail.com>
2024-07-27 09:29:03 -0300

    Changed HTTPStatus to NotFound

--- Injeção de dependências/calculator/src/main/java/br/com/app/CRUD/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ Injeção de dependências/calculator/src/main/java/br/com/app/CRUD/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,7 @@
 package br.com.app.CRUD.exceptions.handler;
 
 import br.com.app.CRUD.exceptions.ExceptionResponse;
-import br.com.app.CRUD.exceptions.UnsupportedMathOperationException;
+import br.com.app.CRUD.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -27,8 +27,8 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(
             Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
@@ -36,6 +36,6 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 request.getDescription(false)
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
