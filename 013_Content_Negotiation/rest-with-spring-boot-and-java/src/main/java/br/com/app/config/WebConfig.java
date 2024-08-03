0bf5deafc2ec2b246461699212d4c03ec2f8cca5
Ângelo Artur <ang.vianna@gmail.com>
2024-08-02 21:00:35 -0300

    body and header parameters

---
+++ 013_Content_Negotiation/rest-with-spring-boot-and-java/src/main/java/br/com/app/config/WebConfig.java
@@ -0,0 +1,28 @@
+package br.com.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        //Via queryParam http://localhost:8080/person?mediaType=xml
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType").ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+}
---
+++ 013_Content_Negotiation/rest-with-spring-boot-and-java/src/main/java/br/com/app/repositories/PersonRepository.java
@@ -0,0 +1,8 @@
+package br.com.app.repositories;
+
+import br.com.app.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {}
